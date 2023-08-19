3263d4501fd428cb852be2b1787f198b9321f9c7
yaqout salameh <yaqoutsalameh@gmail.com>
2023-08-19 18:45:59 +0300

    Moving the persons state to the PersonList component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,10 @@
 "use client"; //to tell next that this is a client component not a server component
 
 import styles from "./page.module.css";
-import { ChangeEvent, ReactNode, useState } from "react";
-import Person from "@/components/Person";
+import { useState } from "react";
 import PersonList from "@/components/PersonList";
 
-type Person = {
-  id: number;
-  name: string;
-  age: number;
-};
-
 export default function Home() {
-  const [persons, setPersons] = useState<Array<Person>>([
-    { id: 1, name: "Yaqout", age: 22 },
-    { id: 2, name: "Sara", age: 22 },
-    { id: 3, name: "Dema", age: 21 },
-  ]);
-
   const headerStyle = {
     backgroundColor: "black",
     textAlign: "center",
@@ -26,10 +13,6 @@ export default function Home() {
     width: "650px",
     margin: "20px",
     paddingTop: "5px",
-    // ":hover": {
-    //   backgroundColor: "green",
-    //   color: "white",
-    // },
   };
 
   const [showPersons, setShowPersons] = useState(true);
@@ -38,41 +21,12 @@ export default function Home() {
     setShowPersons(!showPersons);
   };
 
-  const nameChangedHandler = (
-    event: ChangeEvent<HTMLInputElement>,
-    id: number
-  ) => {
-    const newList: Array<Person> = [...persons];
-    const index = newList.findIndex((person, index) => {
-      return person.id === id;
-    });
-    newList[index].name = event.target.value;
-    setPersons(newList);
-  };
-
-  const deleteHandler = (id: number) => {
-    const newList = [...persons];
-    const index = newList.findIndex((person, index) => {
-      return person.id === id;
-    });
-    newList.splice(index, 1);
-    setPersons(newList);
-  };
-
-  if (showPersons) {
-    headerStyle.backgroundColor = "peru";
-    // headerStyle.color = "white";
-    // headerStyle[":hover"] = {
-    //   backgroundColor: "blue",
-    //   color: "white",
-    // };
-  }
-
-  /// Changing the list of classes dinamically /// need to be fixed
   const mainClasses = [];
   mainClasses.push(styles.main);
-  if (persons.length <= 2) mainClasses.push(styles.blackBG);
-  if (persons.length <= 1) mainClasses.push(styles.bold);
+  if (showPersons) {
+    mainClasses.push(styles.blackBG);
+    headerStyle.backgroundColor = "peru";
+  }
 
   return (
     <main className={mainClasses.join(" ")}>
@@ -80,15 +34,7 @@ export default function Home() {
         Persons
       </h1>
       This is my first React app
-      <PersonList
-        persons={persons}
-        showPersons={showPersons}
-        nameChangedHandler={(
-          event: ChangeEvent<HTMLInputElement>,
-          id: number
-        ) => nameChangedHandler(event, id)}
-        deleteHandler={(personId) => deleteHandler(personId)}
-      />
+      <PersonList showPersons={showPersons} />
       <button
         type="button"
         className={styles.clickMe}
--- src/components/PersonList.tsx
+++ src/components/PersonList.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, ReactNode } from "react";
+import { ChangeEvent, ReactNode, useState } from "react";
 import Person from "./Person";
 import styles from "./PersonList.module.css";
 
@@ -10,27 +10,49 @@ type Person = {
 
 type Props = {
   showPersons: boolean;
-  persons: Array<Person>;
-  nameChangedHandler: (
-    event: ChangeEvent<HTMLInputElement>,
-    id: number
-  ) => void;
-  deleteHandler: (id: number) => void;
 };
 
 export default function PersonList(props: Props) {
   let personsList: Array<ReactNode> = [];
 
+  const [persons, setPersons] = useState<Array<Person>>([
+    { id: 1, name: "Yaqout", age: 22 },
+    { id: 2, name: "Sara", age: 22 },
+    { id: 3, name: "Dema", age: 21 },
+    { id: 4, name: "Ahmad", age: 9 },
+  ]);
+
+  const nameChangedHandler = (
+    event: ChangeEvent<HTMLInputElement>,
+    id: number
+  ) => {
+    const newList: Array<Person> = [...persons];
+    const index = newList.findIndex((person, index) => {
+      return person.id === id;
+    });
+    newList[index].name = event.target.value;
+    setPersons(newList);
+  };
+
+  const deleteHandler = (id: number) => {
+    const newList = [...persons];
+    const index = newList.findIndex((person, index) => {
+      return person.id === id;
+    });
+    newList.splice(index, 1);
+    setPersons(newList);
+  };
+
   if (props.showPersons) {
-    personsList = props.persons.map((person: Person) => {
+    personsList = persons.map((person: Person) => {
       return (
         <div className={styles.PersonList}>
           <Person
             key={person.id}
             name={person.name}
             age={person.age}
-            onChange={(event) => props.nameChangedHandler(event, person.id)}
-            onDelete={() => props.deleteHandler(person.id)}
+            onChange={(event) => nameChangedHandler(event, person.id)}
+            onDelete={() => deleteHandler(person.id)}
           />
         </div>
       );
